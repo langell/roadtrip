cefa8bf4df310d461ec18815163101c743f5d4d0
L Angell <lonny.angell@gmail.com>
2026-03-26 10:29:32 -0500

    fix: initial bootstraping

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -6,9 +6,7 @@ const withBundleAnalyzer = () => (config = {}) => config;
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  },
+  typedRoutes: true,
   transpilePackages: ['@roadtrip/ui'],
   webpack: (cfg) => {
     cfg.resolve.fallback = { ...cfg.resolve.fallback, fs: false };
